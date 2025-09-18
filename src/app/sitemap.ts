45342bbddb3183476ad90d267fa5ab6c01eb98da
hambaiki <pavarit.guide@gmail.com>
2025-09-18 15:46:25 +0700

    Various updates (WIP)

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,7 @@
-import { MetadataRoute } from "next";
+import { navbarItems, additionalNavbarItems } from "@/constants/common/navbar";
+import { exercises } from "@/constants/common/exercise";
 
-import { navbarItems } from "@/constants/common/navbar";
+import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl =
@@ -12,5 +13,18 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     lastModified: new Date().toISOString(),
   }));
 
-  return [...routes];
+  const additionalRoutes = Object.values(additionalNavbarItems).map((item) => ({
+    url: `${baseUrl}${item.href}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  // Exercise routes
+  const exerciseRoutes = exercises.map((exercise) => ({
+    url: `${baseUrl}${exercise.href}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  const allRoutes = [...routes, ...additionalRoutes, ...exerciseRoutes];
+
+  return allRoutes;
 }
